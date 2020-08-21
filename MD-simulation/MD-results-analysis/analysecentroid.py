cc67ed3c5db36d17398f507f8c231af9a35ea8ee
Anantha S Rao <aanantha.s.rao@gmail.com>
2020-08-21 19:27:08 +0530

    functional approach

--- MD-simulation/MD-results-analysis/analysecentroid.py
+++ MD-simulation/MD-results-analysis/analysecentroid.py
@@ -1,21 +1,23 @@
 import numpy as np
 
-def find_centroid(centroidinfo_file_path):
+def euclidean_distance(centroid_datafile):
 
-    with open(centroidinfo_file_path,'r') as rf:
-        data = rf.read().split('\n')
+    def find_centroid(centroid_datafile):
 
-    cA = data[8].split()
-    cB = data[9].split()
+        with open(centroid_datafile,'r') as rf:
+            data = rf.readlines()
 
-    cAx, cAy, cAz = float(cA[3][:-1]), float(cA[4][:-1]), float(cA[5][:-1])
-    cBx, cBy, cBz = float(cB[3][:-1]), float(cB[4][:-1]), float(cB[5][:-1])
+        cA = data[-3].split()
+        cB = data[-2].split()
+        
+        cAx, cAy, cAz = float(cA[-3][:-1]), float(cA[-2][:-1]), float(cA[-1][:-1])
+        cBx, cBy, cBz = float(cB[-3][:-1]), float(cB[-2][:-1]), float(cB[-1][:-1])
 
-    hybrid_centroid = {'A':{'cAx':cAx, 'cAy':cAy, 'cAz':cAz},'B':{'cBx':cBx, 'cBy':cBy, 'cBz':cBz}}
+        centroid_data = {'A':{'cAx':cAx, 'cAy':cAy, 'cAz':cAz},'B':{'cBx':cBx, 'cBy':cBy, 'cBz':cBz}}
 
-    return hybrid_centroid
-
-def euclidean_distance(centroid_data):
+        return centroid_data
+    
+    centroid_data = find_centroid(centroid_datafile)
 
     A_posn =  np.array(list(centroid_data['A'].values()))
     B_posn = np.array(list(centroid_data['B'].values()))
@@ -23,12 +25,4 @@ def euclidean_distance(centroid_data):
     distance =np.sqrt(np.sum(abs_diff**2))
 
     return distance
-        
-path = '/home/asr/Documents/Chimera-files/Repeat3/2.swiss-model-5mza-full-files/mdrun/md1-hybrid/Centroid_analysis/centroidinfo'
-
-centroid_data= find_centroid(path)
-distance = euclidean_distance(centroid_data)
-
-print(distance)
-
 
